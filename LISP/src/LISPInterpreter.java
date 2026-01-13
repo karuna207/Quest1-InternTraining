e98de6622ccaa1281d26d7818bc6e479ae576cd0
karunanidhi <karunanidhiayyamperumal@gmail.com>
2026-01-13 14:34:19 +0530

    Entrypoint changed

--- LISP/src/LISPInterpreter.java
+++ LISP/src/LISPInterpreter.java
@@ -1,23 +1,27 @@
-import nodes.Node;
 import visitors.Evaluator;
 import visitors.ExpressionVisitor;
+import nodes.Node;
+import java.util.ArrayList;
+import java.util.List;
 
 
-void main() {
+public class LISPInterpreter{
+    public static void main() {
 
-    List<String> program = new ArrayList<>();
+        List<String> program=new ArrayList<>();
 
-    program.add("(+ 1 2)");
-    program.add("(* 3 4)");
-    program.add("(define x 10)");
-    program.add("(if (> x 5) 1 0)");
+        program.add("(+ 1 2)");
+        program.add("(* 3 4)");
+        program.add("(define x 10)");
+        program.add("(if (> x 5) 1 0)");
 
-    ExpressionVisitor<Object> evaluator = new Evaluator();
+        ExpressionVisitor<Object> evaluator=new Evaluator();
 
-    Parser p1 = new Parser();
-    for (String line : program) {
-        Node node = p1.parse(line);
-        Object result = node.accept(evaluator);
-        IO.println("result is " + result);
+        Parser p1=new Parser();
+        for(String line:program){
+            Node node=p1.parse(line);
+            Object result=node.accept(evaluator);
+            System.out.println("result is "+result);
+        }
     }
 }
